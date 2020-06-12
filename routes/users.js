074ad934a1f0914893daf2102eb59f1fe6569e86
Hitesh Sharma <hitesharma3899@gmail.com>
2020-06-12 17:53:34 +0530

    added user-authentication

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const {User, validate} = require('../models/user');
 const _  = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 
@@ -12,8 +13,13 @@ router.post('/', async (req, res)=>{
 
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
-    await user.save(_.pick(user, ['_id', 'name', 'email']));
-    res.send(user);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    
+    await user.save();
+    
+    const token = user.generateAuthToken();
+    res.header('x-auth-token', token),send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
